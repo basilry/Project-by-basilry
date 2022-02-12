98f10b7cfba43fee420526f16e98075bff85f2e3
basilry <basilry@gmail.com>
2022-02-13 02:29:02 +0900

    add font-awesome

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import styles from "@styles/components/nav.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
-
-const Nav = ({openSide, setOpenSide} : any) => {
-
+const Nav = ({ openSide, setOpenSide }: any) => {
     return (
         <div className={styles.Nav_whole}>
+            <div onClick={() => setOpenSide(!openSide)}>
+                <FontAwesomeIcon icon={faBars} style={{fontSize: "50px"}} />
+            </div>
             <ul>
-                <li onClick={() => setOpenSide(!openSide)}>햄버거</li>
                 <li onClick={() => console.log(1)}>Projects</li>
                 <li onClick={() => console.log(2)}>Study Now</li>
                 <li onClick={() => console.log(3)}>Plan</li>
             </ul>
         </div>
-    );
+    )
 }
-
 
 export default Nav
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+        
             <Component {...pageProps} />
             <ToastContainer position="bottom-right" autoClose={3500} />
         </>
