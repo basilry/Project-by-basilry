9816581d22f0e94a54dc57cb4b67ea025f23559c
basilry <basilry@gmail.com>
2022-02-17 00:08:34 +0900

    main.tsx coding

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,14 +7,14 @@ const Main = () => {
         <Layout>
             <section className={styles.main_whole}>
                 {/* <div> */}
-                    <p>{"Const Life = () => {"}</p>
+                    <p>{"Const MyLife = () => {"}</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;A Law student</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who dreams of becoming</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a real Front-End Developer.</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;진짜</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;프론트엔드 개발자를 꿈꾸는</p>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp;법학도.</p>
-                    <br />
+                    {/* <br /> */}
                     <p>&nbsp;&nbsp;{"return 'Reality'"}</p>
                     <p>{"};"}</p>
                 {/* </div> */}
