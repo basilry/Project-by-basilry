016040c6b84d5459d9b29d481c9c26e750fc10f0
basilry <basilry@gmail.com>
2022-03-02 23:42:16 +0900

    think about color concept..

--- src/pages/category/aboutme.tsx
+++ src/pages/category/aboutme.tsx
@@ -7,6 +7,12 @@ const aboutme = () => {
         <div className={styles.common_wrapper}>
             <div className={styles.common_area}>
                 저는요...
+
+                <div>
+                    배색에 대한 고민
+                    <p>1. 밝은 색상 - 밝은 주황, 짙은 파랑, 짙은 녹색</p>
+                    <p>2. 어두운 색상 - 흰색, 회색, 짙은 회색, 검은색</p>
+                </div>
             </div>
         </div>
     );
